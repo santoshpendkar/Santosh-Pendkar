dbfe1491c5b59bd712afe8b4606fb0614caedd80
Santosh <pendkarsantosh98@gmail.com>
2022-11-15 14:49:44 +0530

    santosh

---
+++ Git_Method/src/java/collection/method/First.java
@@ -0,0 +1,20 @@
+package java.collection.method;
+
+import java.util.ArrayList;
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		ArrayList ar= new ArrayList();
+		
+		ar.add(1);
+		ar.add(3);
+		ar.add("santosh");
+		ar.add("pendkar");
+		
+		System.out.println(ar);
+		
+	}
+
+}
